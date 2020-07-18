bbd19524cce19275f95590a0276708a8f243edc9
Sujith <gdvsujithsagar217@gmail.com>
2020-07-18 19:18:58 +0530

    added Navbar

--- Script/script.js
+++ Script/script.js
@@ -1,2 +1,18 @@
 /*JavaScript*/
 console.log("Testing");
+
+
+/*Side Navbar in Mobile*/
+document.addEventListener('DOMContentLoaded', function() {
+   var elems = document.querySelectorAll('.sidenav');
+   var instances = M.Sidenav.init(elems);
+ });
+
+ // Or with jQuery
+
+ /*$(document).ready(function(){
+   $('.sidenav').sidenav();
+ });*/
+
+
+/*End of Side Navbar in Mobile*/
