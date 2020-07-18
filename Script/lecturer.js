bf278989d1b955acf60a1c5eddf9c0ef11156b7c
RakeshBeesetty <rakeshbeesetty53@gmail.com>
2020-07-19 02:42:39 +0530

    Added lecturer.html

---
+++ Script/lecturer.js
@@ -0,0 +1,8 @@
+/*Side Navbar in Mobile*/
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+  });
+  
+ /*End of Side Navbar in Mobile*/
