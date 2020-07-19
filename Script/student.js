b5c84a470b11d28100dfe5d897e60851f072ac41
ST24 <sumanthst24@gmail.com>
2020-07-19 19:36:52 +0530

    added view exams for student

--- Script/student.js
+++ Script/student.js
@@ -12,3 +12,34 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 /*End of Side Navbar in Mobile*/
+/* function to display exams*/
+
+ function viewExams(){
+   toggleKey=true;
+   console.log("View Exams is pressed");
+   document.getElementById("student_Profile").style.display = "none";
+   var viewexam=document.getElementsByClassName("ViewExams");
+   var i=0;
+   for(i=0;i<viewexam.length;i++){
+     viewexam[i].style.display="block";
+   }
+   console.log("View Exams is enabled Profile is disabled");
+ }
+
+/*end function to display exams*/
+
+/* function to display profile*/
+function displayProfile(){
+  toggleKey=false;
+  console.log("Profile is pressed")
+  var profile =document.getElementById("student_Profile");
+  profile.style.display="block";
+  var viewexam=document.getElementsByClassName("ViewExams");
+  var i=0;
+  for(i=0;i<viewexam.length;i++){
+    viewexam[i].style.display="none";
+  }
+  console.log("Profile is enabled View Exams is disabled");
+}
+
+/* end function to diplay profile*/
