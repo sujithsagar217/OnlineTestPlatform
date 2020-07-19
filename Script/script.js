cc777fc0734accee6396c2aeac6fec35f1a19660
Sujith <gdvsujithsagar217@gmail.com>
2020-07-19 17:05:34 +0530

    in index.html logo,home and login have more functionalites now

--- Script/script.js
+++ Script/script.js
@@ -1,6 +1,8 @@
 /*JavaScript*/
 console.log("Testing");
+/*Variables*/
 
+var toggleKey = true;
 
 /*Side Navbar in Mobile*/
 document.addEventListener('DOMContentLoaded', function() {
@@ -17,13 +19,36 @@ document.addEventListener('DOMContentLoaded', function() {
 
 /*End of Side Navbar in Mobile*/
 
-
 /* To Hide Login Form and Display Default HomePage */
-function displayLoginForm(){
-  console.log("Login Button is pressed");
+function displayDefaultHomePage(){
+  toggleKey=true;
+  console.log("HomePage or Logo Button is pressed");
   var login_block = document.getElementById("loginBlock");
-  login_block.style.display="inline";
-  console.log("Disabled default homepage and enabled login form");
+  login_block.style.display="none";
+  var homepage = document.getElementsByClassName("homePage");
+  var i=0;
+  for (i = 0; i < homepage.length; i++) {
+  homepage[i].style.display="block";
+}
+  console.log("Enabled default homepage and Disabled login form");
 }
 
 /* End of To Hide Login Form and Display Default HomePage */
+
+
+/* To display Login Form and Hide Default HomePage */
+function displayLoginForm(){
+  toggleKey=false;
+  console.log("Login Button is pressed");
+  var login_block = document.getElementById("loginBlock");
+  login_block.style.display="block";
+  var homepage = document.getElementsByClassName("homePage");
+  var i=0;
+  for (i = 0; i < homepage.length; i++) {
+  homepage[i].style.display="none";
+}
+
+  console.log("Disabled default homepage and enabled login form");
+}
+
+/* End of  To display Login Form and Hide Default HomePage  */
