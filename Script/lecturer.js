adfd59ac18778ab58a874f614b97aa6b1f45c42e
ST24 <sumanthst24@gmail.com>
2020-07-22 20:36:17 +0530

    added view results for lecturer

--- Script/lecturer.js
+++ Script/lecturer.js
@@ -20,3 +20,34 @@ document.addEventListener('DOMContentLoaded', function() {
 // var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
 
 // Or with jQuery
+
+//start of view results function
+function viewResults(){
+  var toggleKey=true;
+  console.log("View results is pressed");
+  document.getElementById("faculty_profile").style.display = "none";
+  var viewres=document.getElementsByClassName("search");
+  var i=0;
+  for(i=0;i<viewres.length;i++){
+    viewres[i].style.display="block";
+  }
+  console.log("View results is enabled Profile is disabled");
+}
+//end of view results function
+
+
+//start of display profile function
+function displayProfile(){
+  var toggleKey=false;
+  console.log("Profile is pressed");
+  var profile =document.getElementById("student_profile");
+  profile.style.display="block";
+  var viewexam=document.getElementsByClassName("search");
+  var i=0;
+  for(i=0;i<viewexam.length;i++){
+    viewexam[i].style.display="none";
+  }
+  console.log("Profile is enabled View results is disabled");
+}
+
+//end of display profile section
