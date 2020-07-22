3795143d7e29bf1a4ca0611f3f2aa61a7421ec48
ST24 <sumanthst24@gmail.com>
2020-07-22 15:42:03 +0530

    added side nav for lecturer

--- Script/lecturer.js
+++ Script/lecturer.js
@@ -10,3 +10,13 @@ document.addEventListener('DOMContentLoaded', function() {
   
  /*End of Side Navbar in Mobile*/
  
+ document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems);
+});
+
+// Initialize collapsible (uncomment the lines below if you use the dropdown variation)
+// var collapsibleElem = document.querySelector('.collapsible');
+// var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
+
+// Or with jQuery
